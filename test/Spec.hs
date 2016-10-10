25031714265c73a557a56a9500a28cec409968a4
Lloyd <lloydmeta@gmail.com>
2016-10-10 20:40:36 +0900

    Add benchmarks after_success
    Try bumping GHC
    
    Further refactoring of tests
    
    Formatting

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,23 +3,22 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  let fibSeq = [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ]
-  let fibWithIdx = zip [0 .. ] fibSeq
+
   describe "fibNaive" $ do
     it "returns the proper fib" $ do
-      let checks = do
-            (idx, exptd) <- fibWithIdx
-            return $ fibNaive idx == exptd
-      all id checks `shouldBe` True
+      all id (runChecks fibNaive) `shouldBe` True
+
   describe "fibLinear" $ do
     it "returns the proper fib" $ do
-      let checks = do
-              (idx, exptd) <- fibWithIdx
-              return $ fibLinear idx == exptd
-      all id checks `shouldBe` True
+      all id (runChecks fibLinear) `shouldBe` True
+
   describe "fibClassic" $ do
     it "returns the proper fib" $ do
-        let checks = do
-              (idx, exptd) <- fibWithIdx
-              return $ fibClassic idx == exptd
-        all id checks `shouldBe` True
+        all id (runChecks fibClassic) `shouldBe` True
+
+fibSeq = [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ]
+fibWithIdx = zip [0 .. ] fibSeq
+
+runChecks f = do
+            (idx, exptd) <- fibWithIdx
+            return $ f idx == exptd
