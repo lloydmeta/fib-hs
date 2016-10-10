b689acf4f0a5c25586dc038b6e414fa6a912f882
Lloyd <lloydmeta@gmail.com>
2016-10-10 21:23:01 +0900

    Refactor benchmarks and add more numbers to readme
    Correct run command in README

--- bench/MainBenchmarkSuite.hs
+++ bench/MainBenchmarkSuite.hs
@@ -4,22 +4,10 @@ import Fibonacci
 -- Our benchmark harness.
 main :: IO ()
 main = defaultMain [
-  bgroup "fibNaive" [
-                 bench "1"  $ whnf fibNaive 1
-               , bench "5"  $ whnf fibNaive 5
-               , bench "9"  $ whnf fibNaive 9
-               , bench "11" $ whnf fibNaive 11
-               ],
-  bgroup "fibLinear" [
-                 bench "1"  $ whnf fibLinear 1
-               , bench "5"  $ whnf fibLinear 5
-               , bench "9"  $ whnf fibLinear 9
-               , bench "11" $ whnf fibLinear 11
-               ],
-  bgroup "fibClassic" [
-                 bench "1"  $ whnf fibClassic 1
-               , bench "5"  $ whnf fibClassic 5
-               , bench "9"  $ whnf fibClassic 9
-               , bench "11" $ whnf fibClassic 11
-               ]
+  bgroup "fibNaive" $ benchesFor fibNaive,
+  bgroup "fibLinear" $ benchesFor fibLinear,
+  bgroup "fibClassic" $ benchesFor fibClassic
   ]
+
+benchesFor :: (Int -> Int) -> [Benchmark]
+benchesFor f = map (\to -> bench (show to) $ nf f to) [10, 20, 30]
